fa289d2bd39c3eeeae106a72145a55eb385d3d5c
0xharkirat <65155920+0xharkirat@users.noreply.github.com>
2026-06-25 17:31:07 +1000

    finished site for demo

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -1,9 +1,11 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
+import { fumadocsComponents } from 'tinacms-fumadocs-pkg/components';
 import type { MDXComponents } from 'mdx/types';
 
 export function getMDXComponents(components?: MDXComponents) {
   return {
     ...defaultMdxComponents,
+    ...fumadocsComponents,
     ...components,
   } satisfies MDXComponents;
 }
